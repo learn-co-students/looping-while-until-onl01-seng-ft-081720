485f1f5da58e9611b9b0c6629648144a4c9e4fee
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-20 02:53:39 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,10 @@
 def using_until
   levitation_force = 6
-  until counter == 10
-  puts "Wingardium Leviosa"
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
     levitation_force += 1
-end 
+  end
+end
+
 
 
